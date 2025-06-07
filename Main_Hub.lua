f338245a8d63ba05a4e271c4aacac1d26b9801d9
hiuvc <153337525+hiuvc@users.noreply.github.com>
2025-06-07 12:49:53 +0700

    Update Main_Hub.lua

--- Main_Hub.lua
+++ Main_Hub.lua
@@ -1,17 +1,23 @@
 local Id = game.PlaceId
 
 -- Tạo bảng chứa các ID game hợp lệ
-local validGameIds = {
+local validGameBFIds = {
     [2753915549] = true,
     [4442272183] = true,
     [7449423635] = true
 }
+local validGameLBIds = {
+    [662417684] = true
+}
 
 -- Kiểm tra nếu ID game hợp lệ
-if validGameIds[Id] then
-    -- Tải script nếu game hợp lệ
+if validGameBFIds[Id] then
+    -- Tải script nếu game hợp lệ Blox Fruits
     loadstring(game:HttpGet("https://raw.githubusercontent.com/hiuvc/hiuhub/refs/heads/main/Binini.lua"))()
+elseif validGameLBIds[Id] then
+    -- Tải script nếu game hợp lệ Lucky Block
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/hiuvc/hiuhub/refs/heads/main/LuckyBlockKid.lua"))()
 else
     -- Nếu game không hợp lệ, kick người chơi
-    game.Players.LocalPlayer:kick("NOT SUPPORT!")
+    game.Players.LocalPlayer:Kick("NOT SUPPORT!")
 end

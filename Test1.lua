61787acc3cead232039d1758324b67a1be526f63
hiuvc <153337525+hiuvc@users.noreply.github.com>
2025-12-21 13:04:00 +0700

    Update Test1.lua

--- Test1.lua
+++ Test1.lua
@@ -36,7 +36,6 @@ repeat
 	wait();
 until e;
 local e = (loadstring(game:HttpGet("https://raw.githubusercontent.com/hiuvc/hiuhub/refs/heads/main/LoadUi.lua")))();
-local HopServer = loadstring(game:HttpGet("https://raw.githubusercontent.com/hiuvc/hiuhub/main/HopServerModule.lua"))()
 if game.PlaceId == 2753915549 or game.PlaceId == 85211729168715 then
 	World1 = true;
 elseif game.PlaceId == 4442272183 or game.PlaceId == 79091703265657 then
